b567ee5163e52dd28c07f6c537e0adbe06c3ad9d
秦岭 <7833492+ql_mountain@user.noreply.gitee.com>
2020-10-08 20:57:15 +0800

    tabbar封装

---
+++ src/components/content/MainTabBar.vue
@@ -0,0 +1,41 @@
+<template>
+    <tab-bar>
+      <tab-bar-item path="/home" activeColor="blue">
+        <img slot="item-icon" src="../../assets/img/img_tabbar/index.png" alt="">
+        <img slot="item-icon-active" src="../../assets/img/img_tabbar/index-o.png" alt="">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category" activeColor="blue">
+        <img slot="item-icon" src="../../assets/img/img_tabbar/info.png" alt="">
+        <img slot="item-icon-active" src="../../assets/img/img_tabbar/info-o.png" alt="">
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart" activeColor="blue">
+        <img slot="item-icon" src="../../assets/img/img_tabbar/cart.png" alt="">
+        <img slot="item-icon-active" src="../../assets/img/img_tabbar/cart-o.png" alt="">
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/user" activeColor="blue">
+        <img slot="item-icon" src="../../assets/img/img_tabbar/user.png" alt="">
+        <img slot="item-icon-active" src="../../assets/img/img_tabbar/user-o.png" alt="">  
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar> 
+</template>
+
+<script>
+  import TabBar from "../common/tabbar/TabBar"
+  import TabBarItem from "../common/tabbar/TabBarItem"
+  export default {
+      name:'MainTabBar',
+      components:{
+        TabBar,
+        TabBarItem
+  }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,40 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+const Category = ()=> import('../views/category/Category')
+const Cart = ()=> import('../views/cart/Cart')
+const User = ()=> import('../views/user/User')
+const Home = ()=> import('../views/home/Home')
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '',
+    redirect:'/home'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/home',
+    component: Home
+    
+  },
+  {
+    path:'/category',
+    component:Category
+},
+{
+    path:'/cart',
+    component:Cart
+},
+{
+    path:'/user',
+    component:User
+}
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  mode:'history'
 })
 
 export default router
